9b8dc99ced1a9b0fd3f22eb5d60e9d708a1371ce
NamD <64962991+nd9800@users.noreply.github.com>
2021-06-14 19:56:13 +0700

    Add project files.

---
+++ Practical/Models/EmployeeList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical.Models
+{
+    public class EmployeeList
+    {
+       
+        public string name { get; set; }
+        public string role { get; set; }
+        public string birthyear { get; set; }
+    }
+
+    public class Root
+    {
+        public List<EmployeeList> employee_list { get; set; }
+    }
+
+
+}
